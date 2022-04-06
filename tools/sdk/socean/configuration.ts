cd441650cb5ebe003863efb6b00162c83fb6ec5f
Thomas Proust <thomas.proust89@gmail.com>
2022-04-06 14:54:12 +0900

    add sdk itxs and programs

---
+++ tools/sdk/socean/configuration.ts
@@ -0,0 +1,63 @@
+import { newProgramMap } from '@saberhq/anchor-contrib'
+import { SolanaAugmentedProvider } from '@saberhq/solana-contrib'
+import { PublicKey } from '@solana/web3.js'
+import { EndpointTypes } from '@models/types'
+import { BondingJSON, BondingProgram } from './programs/bonding'
+import { DescendingAuctionJSON, DescendingAuctionProgram } from './programs'
+
+export type SoceanPrograms = {
+  Bonding: BondingProgram
+  DescendingAuction: DescendingAuctionProgram
+}
+
+export type SupportedCluster = Extract<EndpointTypes, 'devnet' | 'mainnet'>
+
+type MultiClusterPubkey = {
+  [key in SupportedCluster]: PublicKey
+}
+
+class SoceanConfiguration {
+  public readonly bondingProgramId: MultiClusterPubkey = {
+    devnet: new PublicKey('76kLhv2TPJ6aCnHMEcm1kHrsBEsqo4wjCp5mz3prThkk'),
+
+    mainnet: new PublicKey('bon4Kh3x1uQK16w9b9DKgz3Aw4AP1pZxBJk55Q6Sosb'),
+  }
+
+  public readonly descendingAuctionProgramId: MultiClusterPubkey = {
+    devnet: new PublicKey('CwuWwv57X9Yerfhkh9oEDJzr1qgyDFYr2mkyZ3HH8jjJ'),
+
+    mainnet: new PublicKey('desGPJHB9jXNpbqrV4j6Zcpvf4Zu8ijyYpowBFVDMtF'),
+  }
+
+  public readonly bondingProgramInstructions = {
+    mintBondedTokens: 242,
+  }
+
+  public readonly descendingAuctionProgramInstructions = {
+    depositToAuctionPool: 177,
+    purchase: 21,
+  }
+
+  public loadPrograms(
+    provider: SolanaAugmentedProvider,
+    cluster: SupportedCluster
+  ): SoceanPrograms {
+    return newProgramMap<SoceanPrograms>(
+      provider,
+
+      {
+        // IDLs
+        Bonding: BondingJSON,
+        DescendingAuction: DescendingAuctionJSON,
+      },
+
+      {
+        // Addresses
+        Bonding: this.bondingProgramId[cluster],
+        DescendingAuction: this.descendingAuctionProgramId[cluster],
+      }
+    )
+  }
+}
+
+export default new SoceanConfiguration()
