9861b5fae68a5f881010ad2f157dcb1b030f5965
Thomas Proust <thomas.proust89@gmail.com>
2022-04-06 15:05:30 +0900

    add itxs form types and list them in Instructions enum

---
+++ hooks/useSoceanPrograms.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import { Wallet } from '@project-serum/common'
+import {
+  SolanaAugmentedProvider,
+  SolanaProvider,
+} from '@saberhq/solana-contrib'
+import soceanConfiguration, {
+  SoceanPrograms,
+} from '@tools/sdk/socean/configuration'
+import useWalletStore from 'stores/useWalletStore'
+
+export default function useSoceanPrograms() {
+  const connection = useWalletStore((s) => s.connection)
+  const wallet = useWalletStore((s) => s.current)
+
+  const [programs, setPrograms] = useState<SoceanPrograms | null>(null)
+
+  useEffect(() => {
+    if (!connection || !wallet) {
+      return
+    }
+
+    const solanaProvider = SolanaProvider.init({
+      connection: connection.current,
+      wallet: wallet as Wallet,
+    })
+
+    if (connection.cluster === 'localnet') {
+      throw new Error('unsupported cluster for Socean programs loading')
+    }
+
+    setPrograms(
+      soceanConfiguration.loadPrograms(
+        new SolanaAugmentedProvider(solanaProvider),
+        connection.cluster
+      )
+    )
+  }, [connection, wallet])
+
+  return {
+    programs,
+  }
+}
