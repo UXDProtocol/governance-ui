be0bd65fd5be56deb767600a63d4c3ec78d179c6
Grégory NEUT <gregory.neut@gmail.com>
2022-04-07 16:51:36 +0200

    refactor governance select

--- pages/dao/[symbol]/proposal/components/GovernedAccountSelect.tsx
+++ pages/dao/[symbol]/proposal/components/GovernedAccountSelect.tsx
@@ -9,6 +9,8 @@ import {
 } from '@utils/tokens';
 import React, { useEffect } from 'react';
 import { getProgramName } from '@components/instructions/programs/names';
+import { ChipIcon } from '@heroicons/react/solid';
+import { CogIcon, CurrencyDollarIcon } from '@heroicons/react/outline';
 
 const GovernedAccountSelect = ({
   onChange,
@@ -20,40 +22,16 @@ const GovernedAccountSelect = ({
   label,
   noMaxWidth,
 }: {
-  onChange;
-  value;
-  error?;
+  onChange: (selected: GovernedMultiTypeAccount) => void;
+  value?: GovernedMultiTypeAccount;
+  error?: string;
   governedAccounts: GovernedMultiTypeAccount[];
-  shouldBeGoverned?;
+  shouldBeGoverned?: boolean;
   governance?: ProgramAccount<Governance> | null | undefined;
-  label?;
+  label?: string;
   noMaxWidth?: boolean;
 }) => {
-  function getLabel(value: GovernedMultiTypeAccount) {
-    if (value) {
-      const accountType = value.governance.account.accountType;
-      switch (accountType) {
-        case GovernanceAccountType.MintGovernanceV1:
-        case GovernanceAccountType.MintGovernanceV2:
-          return getMintAccountLabelComponent(getMintAccountLabelInfo(value));
-        case GovernanceAccountType.TokenGovernanceV1:
-        case GovernanceAccountType.TokenGovernanceV2:
-          return getTokenAccountLabelComponent(
-            value.isSol
-              ? getSolAccountLabel(value)
-              : getTokenAccountLabelInfo(value),
-          );
-        case GovernanceAccountType.ProgramGovernanceV1:
-        case GovernanceAccountType.ProgramGovernanceV2:
-          return getProgramAccountLabel(value.governance);
-        default:
-          return value.governance.account.governedAccount.toBase58();
-      }
-    } else {
-      return null;
-    }
-  }
-  //TODO refactor both methods (getMintAccountLabelComponent, getTokenAccountLabelComponent) make it more common
+  // TODO refactor both methods (getMintAccountLabelComponent, getTokenAccountLabelComponent) make it more common
   function getMintAccountLabelComponent({
     account,
     tokenName,
@@ -62,21 +40,30 @@ const GovernedAccountSelect = ({
     imgUrl,
   }) {
     return (
-      <div className="break-all text-fgd-1">
-        {account && <div className="mb-0.5">{account}</div>}
-        <div className="mb-2">{mintAccountName}</div>
-        <div className="space-y-0.5 text-xs text-fgd-3">
-          {tokenName && (
-            <div className="flex items-center">
-              Token: <img className="flex-shrink-0 h-4 mx-1 w-4" src={imgUrl} />
-              {tokenName}
-            </div>
-          )}
-          <div>Supply: {amount}</div>
+      <div className="flex items-center">
+        <div className="flex">
+          <CogIcon className="w-8 h-8" />
+        </div>
+
+        <div className="mt-1 ml-2 flex flex-col">
+          <span className="mb-0.5">{mintAccountName}</span>
+
+          <div className="space-y-0.5 text-xs text-fgd-3 flex flex-col">
+            {account && <span className="mb-0.5">{account}</span>}
+
+            {tokenName && (
+              <div className="flex items-center">
+                Token: <img className="flex-shrink-0 h-4 w-4" src={imgUrl} />
+                {tokenName}
+              </div>
+            )}
+            <span>Supply: {amount}</span>
+          </div>
         </div>
       </div>
     );
   }
+
   function getTokenAccountLabelComponent({
     tokenAccount,
     tokenAccountName,
@@ -84,32 +71,80 @@ const GovernedAccountSelect = ({
     amount,
   }) {
     return (
-      <div className="break-all text-fgd-1 ">
-        {tokenAccountName && <div className="mb-0.5">{tokenAccountName}</div>}
-        <div className="mb-2 text-fgd-3 text-xs">{tokenAccount}</div>
-        <div className="flex space-x-3 text-xs text-fgd-3">
-          {tokenName && (
-            <div className="flex items-center">
-              Token:
-              <span className="ml-1 text-fgd-1">{tokenName}</span>
+      <div className="flex items-center">
+        <div className="flex">
+          <CurrencyDollarIcon className="w-8 h-8" />
+        </div>
+
+        <div className="mt-1 ml-2 flex flex-col">
+          {tokenAccountName && <div className="mb-0.5">{tokenAccountName}</div>}
+
+          <div className="mb-0.5 text-fgd-3 text-xs">{tokenAccount}</div>
+
+          <div className="flex space-x-3 text-xs text-fgd-3">
+            {tokenName && (
+              <div className="flex items-center">
+                Token:
+                <span className="ml-1 text-fgd-1">{tokenName}</span>
+              </div>
+            )}
+            <div>
+              Bal:<span className="ml-1 text-fgd-1">{amount}</span>
             </div>
-          )}
-          <div>
-            Bal:<span className="ml-1 text-fgd-1">{amount}</span>
           </div>
         </div>
       </div>
     );
   }
+
   function getProgramAccountLabel(val: ProgramAccount<Governance>) {
-    const name = val ? getProgramName(val.account.governedAccount) : '';
+    const name = val
+      ? getProgramName(val.account.governedAccount)
+      : 'Unknown Program';
+
     return (
-      <div className="flex flex-col">
-        {name && <div>{name}</div>}
-        <div>{val?.account?.governedAccount?.toBase58()}</div>
+      <div className="flex items-center">
+        <div className="flex">
+          <ChipIcon className="w-8 h-8" />
+        </div>
+
+        <div className="mt-1 ml-2 flex flex-col">
+          <span className="">{name}</span>
+
+          <span className="text-fgd-3 text-xs">
+            {val?.account?.governedAccount?.toBase58()}
+          </span>
+        </div>
       </div>
     );
   }
+
+  function getLabel(value?: GovernedMultiTypeAccount) {
+    if (!value) {
+      return null;
+    }
+
+    const accountType = value.governance.account.accountType;
+
+    switch (accountType) {
+      case GovernanceAccountType.MintGovernanceV1:
+      case GovernanceAccountType.MintGovernanceV2:
+        return getMintAccountLabelComponent(getMintAccountLabelInfo(value));
+      case GovernanceAccountType.TokenGovernanceV1:
+      case GovernanceAccountType.TokenGovernanceV2:
+        return getTokenAccountLabelComponent(
+          value.isSol
+            ? getSolAccountLabel(value)
+            : getTokenAccountLabelInfo(value),
+        );
+      case GovernanceAccountType.ProgramGovernanceV1:
+      case GovernanceAccountType.ProgramGovernanceV2:
+        return getProgramAccountLabel(value.governance);
+      default:
+        return value.governance.account.governedAccount.toBase58();
+    }
+  }
+
   useEffect(() => {
     if (governedAccounts.length == 1) {
       //wait for microtask queue to be empty
@@ -118,6 +153,7 @@ const GovernedAccountSelect = ({
       });
     }
   }, [JSON.stringify(governedAccounts)]);
+
   return (
     <Select
       label={label}
